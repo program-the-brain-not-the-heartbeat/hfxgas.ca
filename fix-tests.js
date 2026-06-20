3b54262f13131ccd96e91fe33e2c8a101235faaa
Matt <9137721+program-the-brain-not-the-heartbeat@users.noreply.github.com>
2026-06-20 07:42:12 -0300

    chore: remove accidentally committed debug file

--- fix-tests.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('test/index.test.js', 'utf8');
-c = c.replace(/\bSELF\.fetch\b/g, 'workerExports.default.fetch');
-c = c.replace('Routes via SELF.fetch', 'Routes via workerExports.default.fetch');
-fs.writeFileSync('test/index.test.js', c);
-console.log('done');
